d87974fa913bca3e3000477afbd47ac7e7e91a88
Mancz_hdy <dimasalhamdy@gmail.com>
2026-02-16 16:46:59 +0700

    Add files via upload

---
+++ AkunBank.java
@@ -0,0 +1,40 @@
+class Bank{
+    int rekening;
+
+    public Bank(int saldoAwal){
+        rekening = saldoAwal;
+}
+
+public void cekRekening(){
+     System.out.println("Kamu memiliki rekening sebesar Rp " + rekening);
+}
+public void tarikTunai(int tarikTunai){
+        rekening = rekening - tarikTunai;
+        System.out.println("Kamu baru saja menarik tunai sebesar Rp " + tarikTunai);
+}
+public void depositRekening(int jumlahDeposit){
+        rekening = rekening + jumlahDeposit;
+        System.out.println("Kamu baru saja mendepositkan sebesar Rp " + jumlahDeposit);
+    }
+}
+
+public class AkunBank {
+    public static void main(String[] args){
+        Bank rekeningandy = new Bank(30000000);
+
+        rekeningandy.cekRekening();
+
+        rekeningandy.tarikTunai(5000000);
+
+        rekeningandy.cekRekening();
+
+        rekeningandy.tarikTunai(900000);
+
+        rekeningandy.cekRekening();
+
+        rekeningandy.depositRekening(2000000);
+        
+        rekeningandy.cekRekening();
+
+    }
+}
