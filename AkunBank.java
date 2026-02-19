0e5852f6fb78e3c79c23fc6f1ea6f4ecd030c274
Mancz_hdy <dimasalhamdy@gmail.com>
2026-02-19 15:31:08 +0700

    Minor update: ADD If statement to code.

--- AkunBank.java
+++ AkunBank.java
@@ -1,47 +1,63 @@
 class Bank{
     int rekening;
+    boolean adaRekening;
 
-    public Bank(int saldoAwal){
+    public Bank(int saldoAwal, boolean rekeningAda){
         rekening = saldoAwal;
+        adaRekening = rekeningAda;
     }
 
 public void cekRekening(){
+    if(adaRekening){
      System.out.println("Kamu memiliki rekening sebesar Rp " + rekening);
+        }else{
+            System.out.println("Kamu tidak memiliki rekening.");
+        }
     }
+
 public void tarikTunai(int tarikTunai){
-        rekening = rekening - tarikTunai;
-        System.out.println("Kamu baru saja menarik tunai sebesar Rp " + tarikTunai);
+    if(!adaRekening){
+        System.out.println("Kamu tidak memiliki Rekening.");
+        return;
+    }
+
+    if(rekening < 25000){
+        System.out.println("Saldo di rekening mu tidak cukup.");
+        }else{
+            System.out.print("Kamu berhasil menarik tunai.");
+            rekening -= tarikTunai;
+            System.out.println("Kamu baru saja menarik tunai sebesar Rp " + tarikTunai);
+            }
+        
     }
 public void depositRekening(int jumlahDeposit){
-        rekening = rekening + jumlahDeposit;
-        System.out.println("Kamu baru saja mendepositkan sebesar Rp " + jumlahDeposit);
+    if(!adaRekening){
+        System.out.println("Kamu tidak memiliki rekening.");
+        }else{
+            rekening = rekening + jumlahDeposit;
+            System.out.println("Kamu baru saja mendepositkan sebesar Rp " + jumlahDeposit);
+            }
     }
 
 static void ucapanSalam() {
         System.out.println("Selamat datang di Bank Manrica, Senang untuk dipercaya anda.");
     }
-}
+}    
+
 
 public class AkunBank {
     public static void main(String[] args){
-        Bank rekeningandy = new Bank(30000000);
+        Bank rekeningandy = new Bank(30000000, true);
 
         Bank.ucapanSalam();
 
         rekeningandy.cekRekening();
-
         rekeningandy.tarikTunai(5000000);
-
         rekeningandy.cekRekening();
-
         rekeningandy.tarikTunai(900000);
-
         rekeningandy.cekRekening();
-
         rekeningandy.depositRekening(2000000);
-        
         rekeningandy.cekRekening();
 
     }
-
 }
