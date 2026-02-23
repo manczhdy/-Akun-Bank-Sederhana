e5726920fb1a3c61b401b56db9ba228a7e99c465
Mancz_hdy <dimasalhamdy@gmail.com>
2026-02-23 12:37:51 +0700

    Changes in comparison in rekening chaecking

--- AkunBank.java
+++ AkunBank.java
@@ -11,7 +11,69 @@ public void cekRekening(){
     if(adaRekening){
      System.out.println("Kamu memiliki rekening sebesar Rp " + rekening);
         }else{
+            System.out.println("Kamu tidak memiliki rekening.");class Bank{
+    int rekening;
+    boolean adaRekening;
+
+    public Bank(int saldoAwal, boolean rekeningAda){
+        rekening = saldoAwal;
+        adaRekening = rekeningAda;
+    }
+
+public void cekRekening(){
+    if(adaRekening){
+     System.out.println("Kamu memiliki rekening sebesar Rp " + rekening);
+        }else{
             System.out.println("Kamu tidak memiliki rekening.");
+        }
+    }
+
+public void tarikTunai(int tarikTunai){
+    if(!adaRekening){
+        System.out.println("Kamu tidak memiliki Rekening.");
+        return;
+    }
+
+    if(rekening < tarikTunai){
+        System.out.println("Saldo di rekening mu tidak cukup.");
+        }else{
+            System.out.print("Kamu berhasil menarik tunai. ");
+            rekening -= tarikTunai;
+            System.out.println("Kamu baru saja menarik tunai sebesar Rp " + tarikTunai);
+            }
+        
+    }
+public void depositRekening(int jumlahDeposit){
+    if(!adaRekening){
+        System.out.println("Kamu tidak memiliki rekening.");
+        }else{
+            rekening = rekening + jumlahDeposit;
+            System.out.println("Kamu baru saja mendepositkan sebesar Rp " + jumlahDeposit);
+            }
+    }
+
+static void ucapanSalam() {
+        System.out.println("Selamat datang di Bank Manrica, Senang untuk dipercaya anda.");
+    }
+}    
+
+
+public class AkunBank {
+    public static void main(String[] args){
+        Bank rekeningandy = new Bank(5000000,true);
+
+        Bank.ucapanSalam();
+
+        rekeningandy.cekRekening();
+        rekeningandy.tarikTunai(25000000);
+        rekeningandy.cekRekening();
+        rekeningandy.tarikTunai(900000);
+        rekeningandy.cekRekening();
+        rekeningandy.depositRekening(2000000);
+        rekeningandy.cekRekening();
+
+    }
+}
         }
     }
 
@@ -61,3 +123,4 @@ public class AkunBank {
 
     }
 }
+
